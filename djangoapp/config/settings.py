4b4030638cadd1986c7eb1b250da8db5a48647e3
Chang Doe <kontsuyo@users.noreply.github.com>
2025-01-09 21:09:22 +0900

    [add]カスタムユーザーモデルを追加
    Djangoの公式ドキュメントでは、カスタムユーザーモデルを使うことを推奨しているため。

--- djangoapp/config/settings.py
+++ djangoapp/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -160,3 +161,5 @@ LOGGING = {
         },
     },
 }
+
+AUTH_USER_MODEL = "accounts.CustomUser"
